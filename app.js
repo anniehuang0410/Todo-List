92e664ff7a6ca324954cde65510525470ef3a75f
anniehuang0410 <anniehuang0410@gmail.com>
2023-07-02 22:02:40 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
-const port = 3000
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('MongoDB error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('MongoDB connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log('This app is running on port 3000.')
+app.listen(3000, () => {
+  console.log('This app is running on http://localhost:3000')
 })
