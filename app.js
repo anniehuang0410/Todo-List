ff71aa16f3d6676884294b4d086ed8b04824d2b5
anniehuang0410 <anniehuang0410@gmail.com>
2023-07-04 19:02:59 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const Todo = require('./models/todo')
 const app = express()
 
 if(process.env.NODE_ENV !== 'production') {
@@ -21,7 +22,10 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find()
+    .lean()
+    .then(todo => res.render('index', { todo }))
+    .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
