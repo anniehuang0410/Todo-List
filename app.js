98106da772caa9f7700ba9b603e9b69f077f3287
anniehuang0410 <anniehuang0410@gmail.com>
2023-07-04 15:05:55 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -25,24 +25,32 @@ app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 
 // controller
+// 主頁面
 app.get('/', (req, res) => {
   Todo.find()
     .lean() 
     .then(todos => res.render('index', { todos }))
     .catch(error => console.log(error))
 })
-
+//導向「新增」頁面
 app.get('/todos/new', (req, res) => {
   res.render('new')
 })
-
+// 新增，導回主頁面
 app.post('/todos', (req, res) => {
   const name = req.body.name
   Todo.create({ name })
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
-
+// 瀏覽特定項目
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('detail', { todo }))
+    .catch(error => console.log(error))
+})
 app.listen(3000, () => {
   console.log('This app is running on http://localhost:3000')
 })
