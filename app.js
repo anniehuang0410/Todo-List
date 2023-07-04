62d64b6527cf365cab2a934f8b628486ad780d87
anniehuang0410 <anniehuang0410@gmail.com>
2023-07-04 15:39:25 +0800

    feat: add update todo function

--- app.js
+++ app.js
@@ -51,6 +51,28 @@ app.get('/todos/:id', (req, res) => {
     .then((todo) => res.render('detail', { todo }))
     .catch(error => console.log(error))
 })
+// 修改項目頁面
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  Todo.findById(id)
+    .lean()
+    .then(todo => res.render('edit', { todo })) 
+    .catch(error => console.log(error))
+})
+// 修改項目
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  Todo.findById(id)
+      .then(todo => {
+        todo.name = name
+        return todo.save()
+      })
+      .then(() => res.redirect('/'))
+      .catch(error => console.log(error))
+
+})
+
 app.listen(3000, () => {
   console.log('This app is running on http://localhost:3000')
 })
