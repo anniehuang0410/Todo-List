7915229fb841108d60b0f5a410ff917a5d5b4028
anniehuang0410 <anniehuang0410@gmail.com>
2023-07-06 14:10:08 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Todo.find()
     .lean()
+    .sort({ _id: 'asc' })
     .then(todo => res.render('index', { todo }))
     .catch(error => console.log(error))
 })
