02b5ddbbe020fa6b5a3eb510d6043c0fed4e3350
anniehuang0410 <anniehuang0410@gmail.com>
2023-07-06 23:58:06 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,24 +1,13 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
 const methodOverride = require('method-override')
-const Todo = require('./models/todo')
+
 const routes = require('./routes')
+const db = require('./config/mongoose')
+
 const app = express()
 
-if(process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('MongoDB error!')
-})
-db.once('open', () => {
-  console.log('MongoDB connected!')
-})
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,18 +1,7 @@
-const mongoose = require('mongoose')
-const db = mongoose.connection
+const db = require('../../config/mongoose')
 const Todo = require('../todo')
 
-if (process.env.NODE_ENV !== 'production') {
-   require('dotenv').config()
-}
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-db.on('error', () => {
-  console.log('MongoDB error')
-})
 db.once('open', () => {
-  console.log('MongoDB connected!')
   for (let i = 0; i < 10; i++) {
     Todo.create({ name: `name-${i}`})
   }
