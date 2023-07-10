a058a73cb3f03f34639beae398dd22b878dea905
anniehuang0410 <anniehuang0410@gmail.com>
2023-07-10 23:39:04 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 const routes = require('./routes')
 const db = require('./config/mongoose')
 
 const app = express()
 
-
-
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
