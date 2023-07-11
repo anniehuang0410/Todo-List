e89492d9cc60ef0a61ce836bf1a2f6b470824c7c
anniehuang0410 <anniehuang0410@gmail.com>
2023-07-11 15:27:53 +0800

    feat: add user login feature via passport

---
+++ config/passport.js
@@ -0,0 +1,35 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const User = require('../models/user')
+
+module.exports = app => {
+  // Middleware
+  // 初始化 passport 模組
+  app.use(passport.initialize())
+  app.use(passport.session())
+  // Strategies
+  passport.use(new LocalStrategy({ usernameField: 'email' },
+    function (email, password, done) {
+      User.findOne({ email })
+        .then(user => {
+          if (!user) { 
+            return done(null, false, { message: 'The email is not registered.' }); 
+          }
+          if (user.password !== password) { 
+            return done(null, false, { message: 'The email or password is wrong.' }); 
+          }
+          return done(null, user)
+        })
+      }
+  ));
+  // Sessions
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+  passport.deserializeUser((id, done) => {
+    User.findById(id)
+      .lean()
+      .then(user => done(null, user))
+      .catch(err => done(err, null))
+  })
+}
