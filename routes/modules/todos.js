9d1bfebc874cddd4aa0fd26ff897c54a659192db
anniehuang0410 <anniehuang0410@gmail.com>
2023-07-12 07:11:45 +0800

    feat: add relation between todo and user models

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -9,44 +9,49 @@ router.get('/new', (req, res) => {
 })
 // add new todos
 router.post('/', (req, res) => {
-  const names = String(req.body.name).split(',').map(todo => ({ name: todo }))
+  const userId = req.user._id
+  const names = String(req.body.name).split(',').map(todo => ({ name: todo, userId }))
   Todo.insertMany(names)
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 // detail
 router.get('/:id', (req, res) => {
-  const id = req.params.id
-  Todo.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  Todo.findOne({_id, userId})
     .lean()
     .then(todo => res.render('detail', { todo }))
     .catch(error => console.log(error))
 })
 // to edit page
 router.get('/:id/edit', (req, res) => {
-  const id = req.params.id
-  Todo.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  Todo.findOne({ _id, userId })
     .lean()
     .then(todo => res.render('edit', { todo }))
     .catch(error => console.log(error))
 })
 // edit
 router.put('/:id', (req, res) => {
-  const id = req.params.id
   const { name, isDone } = req.body
-  Todo.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  Todo.findOne({ _id, userId })
     .then(todo => {
       todo.name = name
       todo.isDone = isDone === 'on'
       return todo.save()
     })
-    .then(() => res.redirect(`/todos/${id}`))
+    .then(() => res.redirect(`/todos/${_id}`))
     .catch(error => console.log(error))
 })
 // delete
 router.delete('/:id', (req, res) => {
-  const id = req.params.id
-  Todo.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  Todo.findOne({ _id, userId })
     .then(todo => todo.remove())
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
